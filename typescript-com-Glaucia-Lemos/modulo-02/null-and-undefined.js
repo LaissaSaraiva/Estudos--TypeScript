1b921157e33033cb22bcd7859f8df21d29b5741b
Laissa Saraiva <laissa_saraiva@hotmail.com>
2022-07-13 22:59:59 -0300

    Null e undefined

---
+++ typescript-com-Glaucia-Lemos/modulo-02/null-and-undefined.js
@@ -0,0 +1,12 @@
+"use strict";
+// Exemplo: 01 ==> NUll
+let variavelTesteNull = null; // 
+console.log(variavelTesteNull); // null
+console.log(typeof variavelTesteNull); // object
+// Exemplo: 02 ==> Undefined
+let variavelTesteUndefined;
+console.log(variavelTesteUndefined); // undefined
+console.log(typeof variavelTesteUndefined); // undefined
+//Diferenças e Similaridades: null x undefined
+console.log('Exemplo 01:', null == undefined); // compara resultados
+console.log('Exemplo 02:', null === undefined); // compara tipo
