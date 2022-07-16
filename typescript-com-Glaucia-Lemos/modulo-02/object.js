9aa45ccea9f23452e56c253ac331c1829d3dfdff
Laissa Saraiva <laissa_saraiva@hotmail.com>
2022-07-15 23:29:39 -0300

    Object- parte 02 / Finalização do Módulo 02

--- typescript-com-Glaucia-Lemos/modulo-02/object.js
+++ typescript-com-Glaucia-Lemos/modulo-02/object.js
@@ -34,3 +34,38 @@ console.log(onboarding04({
     linguagem: "JavaScript/ TypeScript",
     email: "laissa_saraiva@hotmail.com",
 }));
+function onboarding05(pessoa) {
+    return `Seja bem vinda ${pessoa.nome}!
+  Sua função aqui na empresa será ${pessoa.funcao}.
+  Você trabalhará com a lingaguagem ${pessoa.linguagem} e seu email de acesso será ${pessoa.email}`;
+}
+console.log(onboarding05({
+    nome: "Laissa",
+    funcao: "Software Developer",
+    linguagem: "JavaScript/ TypeScript",
+    email: "laissasc@teste.com",
+}));
+const filha = {
+    nome: 'Laíssa',
+    sobrenome: "Cunha",
+    idade: 31,
+};
+console.log(filha);
+const usuario = {
+    nome: 'Laíssa',
+    email: 'laissasc@teste.com'
+};
+const admin = {
+    nome: 'Laíssa',
+    email: 'laissasc@teste.com',
+    admin: true,
+};
+function acessarSistema(usuario) {
+    return usuario;
+}
+console.log(acessarSistema(usuario));
+console.log(acessarSistema(admin));
+// function acessarSistema( usuario: Usuario): Usuario {
+//   return usuario;
+// }
+// console.log(acessarSistema(usuario));
--- typescript-com-Glaucia-Lemos/modulo-02/object.ts
+++ typescript-com-Glaucia-Lemos/modulo-02/object.ts
@@ -67,3 +67,100 @@ console.log(
     email: "laissa_saraiva@hotmail.com",
   })
 );
+
+
+// Exemplo 06 == > Propriedade 'readonly' ( caso deseje que devs não modifiquem um determinado objeto use o 'readonly')
+
+interface Pessoa05 {
+  nome: string; // propriedades da interface
+  funcao: string;
+  linguagem: string;
+  readonly email: string; // propriedade readonly / no tempo de execução 
+}
+
+function onboarding05(pessoa: Pessoa05) {
+  return `Seja bem vinda ${pessoa.nome}!
+  Sua função aqui na empresa será ${pessoa.funcao}.
+  Você trabalhará com a lingaguagem ${pessoa.linguagem} e seu email de acesso será ${pessoa.email}`;
+}
+console.log(
+  onboarding05({
+    nome: "Laissa",
+    funcao: "Software Developer",
+    linguagem: "JavaScript/ TypeScript",
+    email: "laissasc@teste.com",
+  })
+);
+
+
+// Exemplo 06 == > Tipos de extensões (heranças)
+
+interface Mae {
+  nome: string;
+}
+
+interface Pai {
+  sobrenome: string;
+}
+
+interface Filha extends Mae , Pai {
+idade: number;
+}
+
+const filha: Filha = {
+  nome: 'Laíssa',
+  sobrenome:  "Cunha",
+  idade: 31,
+}
+
+console.log(filha);
+
+
+// Exemplo 08 ==> Tipos de Interseções
+
+interface Cachorro {
+  raca: string;
+}
+
+interface Gato {
+  raca: string;
+}
+
+type Animal = Cachorro & Gato;
+
+
+// Exemplo 09 ==> Generic Objects <T> - vai aceitar todas as infos de usuario e de admin
+
+type Usuario = {
+  nome: string;
+  email: string;
+}
+
+type Admin = {
+  nome: string;
+  email: string;
+  admin: true;
+}
+
+const usuario: Usuario = {
+  nome: 'Laíssa',
+  email: 'laissasc@teste.com'
+}
+
+const admin: Admin = {
+  nome: 'Laíssa',
+  email: 'laissasc@teste.com',
+  admin: true,
+}
+
+function acessarSistema<T>(usuario: T): T {
+  return usuario;
+}
+
+console.log(acessarSistema<Usuario>(usuario));
+console.log(acessarSistema<Admin>(admin));
+
+// function acessarSistema( usuario: Usuario): Usuario {
+//   return usuario;
+// }
+// console.log(acessarSistema(usuario));
