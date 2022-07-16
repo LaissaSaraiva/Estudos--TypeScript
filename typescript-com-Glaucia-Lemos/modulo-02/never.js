ce13da2d76c4a93757e88a53b0e71858034910eb
Laissa Saraiva <laissa_saraiva@hotmail.com>
2022-07-15 21:30:51 -0300

    tipo never

---
+++ typescript-com-Glaucia-Lemos/modulo-02/never.js
@@ -0,0 +1,15 @@
+"use strict";
+// Exemplos 01 ==> NEVER Throw Exception
+function error(message) {
+    throw new Error(message);
+    console.log(error('Erro de Mensagem 01'));
+}
+// Exemplos 02 ==> NEVER Throw Exception
+function reject() {
+    return error("Erro de Mesagem 02");
+    console.log(reject());
+}
+// Exemplos 03 ==> Função que contém loop infinito retorna o tipo 'never'
+// Exemplos 04 ==> Diferenças entre 'never' ou 'void'
+const algumaCoisaVoid = null;
+const algumaCoisaNever = null;
